3626e3d97ab29aa31f808a0b439cf98e7edb42f6
William Rudent <william.rudent@odissey.cc>
2024-04-10 20:26:21 +0200

    Add missing link from cart item to product details

--- assets/vue/views/CartView.vue
+++ assets/vue/views/CartView.vue
@@ -10,18 +10,18 @@
         </div>
         <div class="mt-3">
           <div class="row mb-3 border-top pt-3" v-for="(node, key) in cart" :key="key">
-            <div class="col-2 col-md-1 mb-3 mb-md-0">
+            <router-link :to="{name: 'product', params: {id: node.item.id}}" class="text-decoration-none text-dark col-2 col-md-1 mb-3 mb-md-0">
               <img :src="node.item.image" :alt="node.item.title" style="height: 50px;">
-            </div>
-            <div class="col-10 col-md-5 mb-3 mb-md-0">{{ node.item.title }}</div>
-            <div class="col-4 col-md-2">
+            </router-link>
+            <router-link :to="{name: 'product', params: {id: node.item.id}}" class="text-decoration-none text-dark col-10 col-md-5 mb-3 mb-md-0">{{ node.item.title }}</router-link>
+            <div class="text-decoration-none text-dark col-4 col-md-2">
               <div class="input-group">
                 <button class="btn btn-outline-secondary" type="button" v-on:click="handleDecrease(node.item)">-</button>
-                <input type="text" class="form-control text-center" :value="node.quantity">
+                <input type="text" class="form-control text-center" :value="node.quantity" disabled>
                 <button class="btn btn-outline-secondary" type="button" v-on:click="handleIncrease(node.item)">+</button>
               </div>
             </div>
-            <div class="col-auto ms-auto">{{ node.item.price }} &euro;</div>
+            <router-link :to="{name: 'product', params: {id: node.item.id}}" class="text-decoration-none text-dark col-auto ms-auto">{{ node.item.price }} &euro;</router-link>
             <div class="col-2 d-none d-md-block">
               <button type="button" class="btn btn-danger" v-on:click="handleDelete(node.item)">Delete</button>
             </div>
